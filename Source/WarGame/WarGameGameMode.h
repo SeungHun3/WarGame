b3aeaa9282d1c5dd23936d0ea03a6bd291e289b4
Seunghun <shlee4508@naver.com>
2024-01-03 13:17:53 +0900

    init again

---
+++ Source/WarGame/WarGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WarGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AWarGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AWarGameGameMode();
+};
+
+
+
